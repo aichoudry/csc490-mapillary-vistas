7a4ffe62853634bea43948b12aef7116eace813c
Ryan D'Rosario <ryan.drosario@mail.utoronto.ca>
2023-11-20 00:44:56 -0500

    Changed evaluation.py to jaccard_index function.

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,5 @@
 import torch
+from torchmetrics.classification import MulticlassJaccardIndex
 
 def iou_single_class(pred, target):
     intersection = (pred & target).float().sum()
@@ -29,7 +30,8 @@ def evaluate_model(model, dataloader, device, num_classes):
                 print(f"Testing Image: {i}/{len(dataloader)}")
             images, masks = images.to(device), masks.to(device)
             outputs = model(images)
-            total_mIoU += mean_iou(outputs, masks, num_classes)
+            jaccard = MulticlassJaccardIndex(num_classes=num_classes).to(device)
+            total_mIoU += jaccard(outputs, masks)
 
     mean_mIoU = total_mIoU / len(dataloader)
     return mean_mIoU
