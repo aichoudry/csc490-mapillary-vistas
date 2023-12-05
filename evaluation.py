f97a2dbcdccf0b59b9ff3d5ff5f955ec5ecd6b9e
aichoudry <25648715+aichoudry@users.noreply.github.com>
2023-12-05 16:45:02 -0500

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,5 @@
 import torch
+from torchmetrics.classification import MulticlassJaccardIndex
 
 def iou_single_class(pred, target):
     intersection = (pred & target).float().sum()
@@ -26,10 +27,11 @@ def evaluate_model(model, dataloader, device, num_classes):
     with torch.no_grad():
         for i, (images, masks) in enumerate(dataloader):
             if i % 5 == 0:
-                print(f"Testing Image: {i}/{len(dataloader)}")
+                print(f"Testing Image: {i}/{len(dataloader)} {total_mIoU / len(dataloader)}")
             images, masks = images.to(device), masks.to(device)
             outputs = model(images)
-            total_mIoU += mean_iou(outputs, masks, num_classes)
+            jaccard = MulticlassJaccardIndex(num_classes=num_classes).to(device)
+            total_mIoU += jaccard(outputs, masks)
 
     mean_mIoU = total_mIoU / len(dataloader)
     return mean_mIoU
